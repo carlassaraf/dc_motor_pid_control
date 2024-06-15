a9f5b5be8285541932342a52f628a6726c50c1db
Fabrizio Carlassara <fabrizio.carlassara@gmail.com>
2024-06-14 23:00:09 -0300

    feat: agrego funcion para puerto serie

--- pid_plotter/pid_plotter.py
+++ pid_plotter/pid_plotter.py
@@ -1,4 +1,6 @@
 import dearpygui.dearpygui as dpg
+import serial.tools.list_ports
+import serial
 
 
 class PIDPlotter:
@@ -16,8 +18,26 @@ class PIDPlotter:
         dpg.create_context()
         dpg.create_viewport(title="PID Plotter", width=width, height=height)
 
+        # Puerto serial
+        self._port = None
+
         # Configuro una ventana para el ploteo del PID
         with dpg.window(label="App", tag="app_window"):
+
+            # Crear la ventana de selección de puerto serial
+            with dpg.child_window(tag="serial_window"):
+                dpg.add_text("Seleccione un puerto serial:")
+                
+                # Listar los puertos seriales disponibles y crear el combo box
+                ports = [port.device for port in serial.tools.list_ports.comports()]
+
+                if ports:
+                    dpg.add_combo(ports, label="Puertos", callback=self._port_selected_callback, tag="serial_combo")
+                else:
+                    dpg.add_text("No se encontraron puertos seriales.")
+
+                dpg.add_text("", tag="serial_status")
+                
             # Vista para el grafico del PID
             with dpg.child_window(tag="position_window"):
                 # Ventana de plot
@@ -45,6 +65,7 @@ class PIDPlotter:
     def run(self):
         while dpg.is_dearpygui_running():
             self._update_plot()
+            self._refresh_ports()
             dpg.render_dearpygui_frame()
 
         dpg.destroy_context()
@@ -61,9 +82,31 @@ class PIDPlotter:
         """
         # Obtengo las dimensiones del viewport y las configuro para la ventana
         width, height = dpg.get_viewport_client_width() - 22.5, dpg.get_viewport_client_height() - 50
-        # dpg.set_item_width("serial_window", width)
-        # dpg.set_item_height("serial_window", height // 7)
+        dpg.set_item_width("serial_window", width)
+        dpg.set_item_height("serial_window", height // 7)
         dpg.set_item_width("position_window", width)
         dpg.set_item_height("position_window", 3 * height // 7)
         dpg.set_item_width("pwm_window", width)
         dpg.set_item_height("pwm_window", 3 * height // 7)
+
+    def _refresh_ports(self):
+        # Listar los puertos seriales disponibles y crear el combo box
+        ports = [port.device for port in serial.tools.list_ports.comports()]
+        if ports:
+            dpg.configure_item("serial_combo", items=ports)
+        else:
+            dpg.configure_item("serial_combo", items=["No se encontraron puertos seriales."])
+
+    def _port_selected_callback(self, sender, app_data):
+        """
+        Obtiene el valor seleccionado del menu desplegable
+        """
+        if self._port:
+            self._port.close()
+            self._port = None
+        else:
+            try:
+                self._port = serial.Serial(app_data, 115200)
+                dpg.set_value(item="serial_status", value=f"Puerto {app_data} conectado con exito!")
+            except:
+                dpg.set_value(item="serial_status", value="Error conectando al puerto!")
