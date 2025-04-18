1df315575d399ba5b427bcdfdbccba73a38d1cf2
Fabrizio Carlassara <fabrizio.carlassara@gmail.com>
2025-04-18 15:12:06 -0300

    feat: plotter initialization and pid constants setting

--- mcu_code/mcu_code.c
+++ mcu_code/mcu_code.c
@@ -26,27 +26,34 @@ int main(void) {
 
   // PID initialization
   pid_init(&(pid_config_t){
-    .kp = 2,
-    .ki = 0,
-    .kd = 0,
-    .ts = 200,
-    .ref = 2000,
-    .out_min = -1000,
-    .out_max = 1000,
+    .kp = APP_KP,
+    .ki = APP_KI,
+    .kd = APP_KD,
+    .ts = 1,
+    .ref = 2500,
+    .out_min = -20000,
+    .out_max = 20000,
     .cb = sampling
   });
+  pid_plotter_init(100);
 
   // Motor driver initialization
   l298_t l298 = {
-    .dir1_pin = 16,
-    .dir2_pin = 17,
-    .en_pin = 18
+    .dir1_pin = 12,
+    .dir2_pin = 13,
+    .en_pin = 14
   };
   l298_init(l298);
 
   // PID constant selector
   gpio_init(OPT_PIN);
   gpio_set_dir(OPT_PIN, false);
+  gpio_pull_up(OPT_PIN);
+
+  // Avoid turning on built in LED
+  gpio_init(16);
+  gpio_set_dir(16, true);
+  gpio_put(16, false);
 
   // Initialize ADC and channels
   adc_init();
@@ -58,23 +65,21 @@ int main(void) {
   while (true) {
     // Constants from memory or potentiometer?
     if(gpio_get(OPT_PIN)) {
-      // Read potentiometer scaled values
-      float kp = get_kp();
-      float ki = get_ki();
-      float kd = get_kd();
-      // Update PID constants
-      pid_update_constants(kp, ki, kd);
+      pid_update_constants(get_kp(), get_ki(), get_kd(), get_ref());
+    }
+    else {
+      pid_update_constants(APP_KP, APP_KI, APP_KD, get_ref());
     }
 
     // Update PWM output from PID output
     int16_t pwm_output = TO_PER(pid_get_output());
     // Check direction
-    if(pwm_output > 0) {
+    if(pwm_output < 0) {
       // Clockwise direction
       l298_set_dir(l298, true);
-      l298_set_speed(l298, pwm_output);
+      l298_set_speed(l298, ABS(pwm_output));
     }
-    else if(pwm_output < 0) {
+    else if(pwm_output > 0) {
       // Counter clockwise direction
       l298_set_dir(l298, false);
       l298_set_speed(l298, ABS(pwm_output));
@@ -84,17 +89,5 @@ int main(void) {
       l298_stop(l298);
       l298_set_speed(l298, 0);
     }
-
-    // Get data to plot
-    pid_to_plotter_t d = pid_get_plot_data();
-
-    printf("{\"kp\":%f,\"ki\":%f,\"kd\":%f,\"ts\":%f,\"reference\":%f,\"position\":%f,\"error\":%f,\"pwm\":%f}\n", 
-        d.kp, d.ki, d.kd, d.ts,
-        TO_DEG(d.ref) - 180, 
-        TO_DEG(d.pos) - 180, 
-        TO_DEG(d.err), 
-        TO_PER(d.out)
-    );
-    sleep_ms(200);
   }
 }
