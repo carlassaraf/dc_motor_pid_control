94f02f25309f0491b80a40a1cd1449b008bdb77a
Fabrizio Carlassara <fabrizio.carlassara@gmail.com>
2025-07-08 08:14:44 -0300

    feat: migracion a tareas de freertos

--- mcu_code/mcu_code.c
+++ mcu_code/mcu_code.c
@@ -6,13 +6,7 @@
 #include "pid.h"
 #include "as5600.h"
 #include "macros.h"
-
-/**
- * @brief Callback for PID sampling
- */
-float sampling(void) {
-  return as5600_get_raw_angle();
-}
+#include "app_tasks.h"
 
 /**
  * @brief Programa principal
@@ -21,73 +15,17 @@ int main(void) {
 
   stdio_init_all();
 
-  // Magnetic encoder initialization
-  as5600_init(i2c0, SDA_PIN, SCL_PIN);
-
-  // PID initialization
-  pid_init(&(pid_config_t){
-    .kp = APP_KP,
-    .ki = APP_KI,
-    .kd = APP_KD,
-    .ts = 1,
-    .ref = 2500,
-    .out_min = -20000,
-    .out_max = 20000,
-    .cb = sampling
-  });
-  pid_plotter_init(100);
-
   // Motor driver initialization
   l298_t l298 = {
     .dir1_pin = 12,
     .dir2_pin = 13,
     .en_pin = 14
   };
-  l298_init(l298);
 
-  // PID constant selector
-  gpio_init(OPT_PIN);
-  gpio_set_dir(OPT_PIN, false);
-  gpio_pull_up(OPT_PIN);
+  xTaskCreate(task_init, "Init", tskINIT_STACK, (void*) &l298, tskINIT_PRIO, NULL);
+  xTaskCreate(task_pid_constants, "PID Const", tskPID_CONS_STACK, NULL, tskPID_CONS_PRIO, NULL);
+  xTaskCreate(task_pid_output, "PID Out", tskPID_OUT_STACK, (void*) &l298, tskPID_OUT_PRIO, NULL);
 
-  // Avoid turning on built in LED
-  gpio_init(16);
-  gpio_set_dir(16, true);
-  gpio_put(16, false);
-
-  // Initialize ADC and channels
-  adc_init();
-  adc_gpio_init(KP_POT);
-  adc_gpio_init(KI_POT);
-  adc_gpio_init(KD_POT);
-  adc_gpio_init(REF_POT);
-
-  while (true) {
-    // Constants from memory or potentiometer?
-    if(gpio_get(OPT_PIN)) {
-      pid_update_constants(get_kp(), get_ki(), get_kd(), get_ref());
-    }
-    else {
-      pid_update_constants(APP_KP, APP_KI, APP_KD, get_ref());
-    }
-
-    // Update PWM output from PID output
-    int16_t pwm_output = TO_PER(pid_get_output());
-    // Check direction
-    if(pwm_output < 0) {
-      // Clockwise direction
-      l298_set_dir(l298, true);
-      l298_set_speed(l298, ABS(pwm_output));
-    }
-    else if(pwm_output > 0) {
-      // Counter clockwise direction
-      l298_set_dir(l298, false);
-      l298_set_speed(l298, ABS(pwm_output));
-    }
-    else {
-      // Stop
-      l298_stop(l298);
-      l298_set_speed(l298, 0);
-    }
-  }
+  vTaskStartScheduler();
+  while (true);
 }
